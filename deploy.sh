578f3c3a05d7d249b066236c5b354c2a23044781
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-02-09 14:40:03 +0100

    New laser screens.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,13 @@ rsync -e ssh -av \
     --exclude=deploy.sh* \
     --exclude=.git* \
     --exclude=*~ \
+    --exclude=freia-symbols \
     --exclude=README.md \
     . konrad@freia.physics.uu.se:/var/www/CSS/dbwrDir
+
+rsync -e ssh -av \
+    --exclude=install.sh* \
+    --exclude=.git* \
+    --exclude=*~ \
+    --exclude=README.md \
+    freia-symbols/ konrad@freia.physics.uu.se:/var/www/CSS/dbwrDir/freia-symbols
