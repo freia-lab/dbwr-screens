7e0cf1d8b5c61667697d8b429a0d5d27d83f5449
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-06-15 09:31:20 +0200

    Modified group id of the files to be synched on freia.physics.uu.se to allow deploy the files by all users belonging to adm group.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+chgrp -R adm *
+chgrp -R adm freia-symbols/*
 rsync -e ssh -av --delete \
     --exclude=deploy.sh* \
     --exclude=.git* \
     --exclude=*~ \
     --exclude=freia-symbols \
     --exclude=README.md \
-    . konrad@freia.physics.uu.se:/var/www/CSS/dbwrDir
+    . $USER@freia.physics.uu.se:/var/www/CSS/dbwrDir
 
 rsync -e ssh -av --delete \
     --exclude=install.sh* \
     --exclude=.git* \
     --exclude=*~ \
     --exclude=README.md \
-    freia-symbols/ konrad@freia.physics.uu.se:/var/www/CSS/dbwrDir/freia-symbols
+    freia-symbols/ $USER@freia.physics.uu.se:/var/www/CSS/dbwrDir/freia-symbols
